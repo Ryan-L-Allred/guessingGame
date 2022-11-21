ab5d5043e05dade338dc22152a82ebfed41570a3
Ryan Allred <ryanallred91@gmail.com>
2022-11-21 09:36:12 -0600

    guessingGame phase 1

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user! Guess the secret number,n");
+            Console.Write("What's you guess? input it here:");
+            
+            string UserAnswer = Console.ReadLine();
+            
+            Console.Write(UserAnswer);
+            // Console.ReadLine();
+        }
+    }
+}
